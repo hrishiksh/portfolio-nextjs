3b1c202b23ed647229028840b2293c20d5302407
hrishiksh <hrishi1pathak@gmail.com>
2022-01-15 20:04:57 +0530

    ADD: code comparison system like github is added. now it highlight ++ or -- tagged codeline

--- components/CodeBlock.jsx
+++ components/CodeBlock.jsx
@@ -21,15 +21,46 @@ const CodeBlock = ({ code, language }) => {
       >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
           <pre
-            className={`${className} font-inconsolata text-base sm:text-lg px-8 pt-8 rounded-md bg-gray-100 overflow-auto my-4 dark:bg-gray-700/50`}
+            className={`${className} font-inconsolata text-base sm:text-lg pt-8 rounded-md bg-gray-100 overflow-auto my-4 dark:bg-gray-700/50`}
           >
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
+            {tokens.map(function (line, i) {
+              if (
+                line[0]?.content.includes("++") ||
+                line[1]?.content.includes("++")
+              ) {
+                return (
+                  <div
+                    {...getLineProps({ line, key: i })}
+                    className="bg-green-500/20 pl-1 pr-8"
+                  >
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                );
+              } else if (
+                line[0]?.content.includes("--") ||
+                line[1]?.content.includes("--")
+              ) {
+                return (
+                  <div
+                    {...getLineProps({ line, key: i })}
+                    className="bg-red-500/20 pl-1 pr-8"
+                  >
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                );
+              }
+              return (
+                <div {...getLineProps({ line, key: i })} className="px-8">
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              );
+            })}
           </pre>
         )}
       </Highlight>
