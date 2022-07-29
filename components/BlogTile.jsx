40f1f0124abc602ed0ccc27d5b12dc88eea86e30
hrishiksh <hrishi1pathak@gmail.com>
2022-07-29 11:40:37 +0530

    Some UI changes

--- components/BlogTile.jsx
+++ components/BlogTile.jsx
@@ -3,13 +3,13 @@ import useDateParser from "../hooks/useDateParser";
 const BlogTile = ({ title, thumbnail, author, date }) => {
   let dateParser = useDateParser();
   return (
-    <div>
+    <div className="py-6">
       <img
         className="w-full object-cover rounded"
         src={thumbnail}
         alt={title}
       />
-      <h2 className="font-inter font-semibold text-lg sm:text-xl py-6 sm:py-0 sm:pt-6 dark:text-gray-100 text-center">
+      <h2 className="font-inter font-semibold text-lg pt-4 sm:text-xl sm:pt-6 dark:text-gray-100 text-center">
         {title}
       </h2>
       <p className="font-inter font-normal text-slate-500 pt-2 text-sm text-center">
